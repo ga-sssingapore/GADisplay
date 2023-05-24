346116104611429346543827c6a241cf61cdd5a5
Tan Justinn <ohhoehoe@gmail.com>
2023-05-24 23:08:45 +0800

    Remade back-end folder due to directory errors

--- back/server.py
+++ back/server.py
@@ -1,15 +1,23 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+import datetime
 load_dotenv()
 
 app = Flask(__name__)
 
+data = [
+    {
+        "name": "Justinn",
+        "registration_date": datetime.datetime.now()
+    }
+]
+
+
 @app.route("/")
-def test():
-    return "<p>Hello, World!</p>"
+def get_data():
+    return jsonify(data)
 
 # @app.route("/login", methods="POST")
 # def login():
 #     if request.method == "POST":
 #         print(request)
-
