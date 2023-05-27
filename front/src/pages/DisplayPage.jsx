65c7da61e9451e00d3d4c6b2fd6f1394907abe6e
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 04:26:54 +0800

    Draft classroom display page

--- front/src/pages/DisplayPage.jsx
+++ front/src/pages/DisplayPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 function DisplayPage() {
-  return <div>DisplayPage</div>;
+  const { number } = useParams();
+  return (
+    <div>
+      <div>Classroom {number}</div>
+      <div>$batch</div>
+      <div>$time</div>
+      <hr />
+      <div>
+        <img src="" alt="GA logo" />
+        <div>GENERAL ASSEMBLY</div>
+      </div>
+    </div>
+  );
 }
 
 export default DisplayPage;
