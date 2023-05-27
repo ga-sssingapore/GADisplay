b3422e32519fd5be09dff2d1fdc2ae6584b47ecb
Tan Justinn <ohhoehoe@gmail.com>
2023-05-27 19:51:40 +0800

    Added db connection to admin routes

--- back/blueprints/admin/routes.py
+++ back/blueprints/admin/routes.py
@@ -1,8 +1,16 @@
+import psycopg2
+import os
 from flask import Blueprint
 from middleware.auth import login_required
 
-
 admin_bp = Blueprint('admin_bp', __name__, url_prefix='/admin')
+
+def get_users_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='ga_users_db',
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
 @admin_bp.route('/user')
 @login_required
