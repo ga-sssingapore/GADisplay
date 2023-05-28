a6776f6ce5c7e95a30a8f78f293c7b3d494414af
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 14:47:14 +0800

    Added input fields for login page

--- front/src/pages/AdminLoginPage.jsx
+++ front/src/pages/AdminLoginPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function AdminLoginPage() {
-  return <div>AdminLoginPage</div>;
+  return (
+    <>
+      <div>Log In</div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input type="text" id="email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" />
+      </div>
+    </>
+  );
 }
 
 export default AdminLoginPage;
