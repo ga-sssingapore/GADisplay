b07332209abdebb14e9de2dae769f83eea06eb08
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 16:30:08 +0800

    Styled landing page
    -Coloured similarly to display page

--- front/src/pages/LandingPage.jsx
+++ front/src/pages/LandingPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import RoomDisplayList from "./components/RoomDisplayList";
 import { Link } from "react-router-dom";
+import styles from "./pages_css/LandingPage.module.css";
 
 function LandingPage() {
   return (
-    <div>
+    <div className={styles.container}>
       <RoomDisplayList />
       <hr />
       <Link to="/admin">
-        <img src="/ga.ico" alt="GA icon login" />
+        <img
+          src="/GA_banner_horizontal.png"
+          alt="GA icon login"
+          className={styles.banner}
+        />
       </Link>
     </div>
   );
