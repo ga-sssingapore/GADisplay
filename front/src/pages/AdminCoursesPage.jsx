08f1e4e41c2c48b7b1dfd7d58b77f95010d4f666
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 21:31:47 +0800

    Setup Courses page

--- front/src/pages/AdminCoursesPage.jsx
+++ front/src/pages/AdminCoursesPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./pages_css/AdminCoursesPage.module.css";
+import CoursesList from "./components/CoursesList";
 
 function AdminCoursesPage() {
-  return <div>AdminCoursesPage</div>;
+  return (
+    <div>
+      <div className={styles.notes}>*Completed courses are filtered out</div>
+      <CoursesList />
+    </div>
+  );
 }
 
 export default AdminCoursesPage;
