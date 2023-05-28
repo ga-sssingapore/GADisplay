8eaf351fd1f9b717f8fb859c80262c23db8aba7a
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 14:46:24 +0800

    Added tablet viewport width to theme

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+/**
+ * Tablet: Samsung Galaxy Tab A
+ * Screen resolution: 1920, 1200
+ * Width when vertical = 1200
+ * pixel per inch = 2.16, rounded
+ * Viewport width when in use = vertical width / ppi = 1200 / 2.16 = 555.55px
+ */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "556px",
+    },
   },
   plugins: [],
 };
