6f63bb51bd98aacfe2cb8566a2c92d8fa2fac5da
Tan Justinn <ohhoehoe@gmail.com>
2023-05-28 23:26:49 +0800

    Added date-picker to dashboard

--- front/src/pages/AdminDashboardPage.jsx
+++ front/src/pages/AdminDashboardPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./pages_css/AdminDashboardPage.module.css";
+import Weekalendar from "./components/Weekalendar";
 
 function AdminDashboardPage() {
-  return <div>AdminDashboardPage</div>;
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
+
+  function handleDateChange(event) {
+    setDate(event.target.value);
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.datepicker}>
+        <label htmlFor="date_picker">Date:</label>
+        <input
+          type="date"
+          id="date_picker"
+          value={date}
+          onChange={handleDateChange}
+        />
+      </div>
+      <Weekalendar date={new Date(date)} />
+    </div>
+  );
 }
 
 export default AdminDashboardPage;
