4068b165b274f6f527b124cf768549e0cf26b6df
Tan Justinn <ohhoehoe@gmail.com>
2023-05-30 19:14:36 +0800

    Added default values to Users

--- back/models/users.py
+++ back/models/users.py
@@ -1,13 +1,14 @@
+from uuid import uuid4
 from server import db
 
 class Users(db.Model):
     __tablename__='Users'
 
-    id = db.Column(db.Uuid, primary_key=True)
+    id = db.Column(db.Uuid, primary_key=True, default=uuid4())
     name = db.Column(db.String(255))
     email = db.Column(db.String(255))
     hash = db.Column(db.String(255))
-    role = db.Column(db.ForeignKey('Roles.role'))
+    role = db.Column(db.ForeignKey('Roles.role'), default='Registered')
 
     def __init__(self, name, email, hash, role):
         self.name = name
