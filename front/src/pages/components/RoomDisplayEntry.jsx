ce2000782de9038356907467398c990688c00def
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-30 10:47:13 +0800

    Changed font-sizes to rem for better responsiveness

--- front/src/pages/components/RoomDisplayEntry.jsx
+++ front/src/pages/components/RoomDisplayEntry.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function RoomDisplayEntry(props) {
   return (
     <Link to={`/display/${props.number}`}>
-      <div className={props.styles.display_link}>CLASSROOM {props.number}</div>
+      <div className={props.styles.display_link}>ROOM {props.number}</div>
     </Link>
   );
 }
