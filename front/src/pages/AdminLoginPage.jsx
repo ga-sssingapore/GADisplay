674523489ce96622bcf37cd9237433b5bfed7992
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-30 11:28:54 +0800

    Added register button to login page

--- front/src/pages/AdminLoginPage.jsx
+++ front/src/pages/AdminLoginPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from "./pages_css/AdminLoginPage.module.css";
+import { Link } from "react-router-dom";
 
 function AdminLoginPage() {
   return (
     <>
-      <div>Log In</div>
+      <img
+        src="/GA_banner_horizontal_black.png"
+        alt="GA banner"
+        className={styles.banner}
+      />
       <div>
         <label htmlFor="email">Email: </label>
         <input type="text" id="email" />
@@ -11,6 +17,12 @@ function AdminLoginPage() {
       <div>
         <label htmlFor="password">Password: </label>
         <input type="password" id="password" />
+      </div>
+      <button>Log In</button>
+      <hr />
+      <div className={styles.register_prompt}>
+        Require access? Please <Link to="/admin/register">register</Link> and
+        inform an admin
       </div>
     </>
   );
