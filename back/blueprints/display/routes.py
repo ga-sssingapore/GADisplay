df60014b2ff15e5616fb32850a7a5ae723985a99
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-31 16:19:35 +0800

    Experimental get_registered method implemented
    -Test method exploring utility of marshmallow Schemas

--- back/blueprints/display/routes.py
+++ back/blueprints/display/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from argon2 import PasswordHasher
 from models.db import db
 from models.users import Users
+from schemas.users import UsersSchema
+from models.roles import Roles
 
 display_bp = Blueprint('display_bp', __name__, url_prefix='/display')
 
@@ -15,3 +17,11 @@ def check_user():
         return jsonify({"status": "ok", "message": ver})
     else:
         return jsonify({"status": "error", "message": "no user"})
+
+
+@display_bp.route('/test2')
+def get_registered():
+    registered_users = db.session.query(Users).filter(Users.role=='Registered').all()
+    role_schema = UsersSchema(many=True)
+    dump_roles = role_schema.dump(registered_users)
+    return jsonify(dump_roles)
