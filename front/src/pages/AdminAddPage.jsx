2af2bae151e4faefe87b36883b85379dc193c451
Tan Justinn <ohhoehoe@gmail.com>
2023-05-31 22:50:31 +0800

    Drafted Add and Adhoc page

---
+++ front/src/pages/AdminAddPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AdminAddPage() {
+  return (
+    <div>
+      <div>Course Code</div>
+      <div>Course Type</div>
+      <div>Start Date</div>
+      <div>End Date</div>
+      <div>Days on campus</div>
+      <div>Start Time</div>
+      <div>End Time</div>
+      <div>Room</div>
+      <div>Import CSV</div>
+    </div>
+  );
+}
+
+export default AdminAddPage;
--- front/src/pages/AdminAdhocPage.jsx
+++ front/src/pages/AdminAdhocPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function AdminAdhocPage() {
-  return <div>AdminAdhocPage</div>;
+  // Also record logged in user who is performing the booking
+  return (
+    <div>
+      <div>Event</div>
+      <div>Date</div>
+      <div>Start Time</div>
+      <div>End Time</div>
+      <div>Room</div>
+      <div>Purpose</div>
+    </div>
+  );
 }
 
 export default AdminAdhocPage;
