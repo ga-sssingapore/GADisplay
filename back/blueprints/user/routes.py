3b8da65e23f3a2f9b8c8c4747999df85d714bbf7
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-31 11:59:28 +0800

    Added temp register user

---
+++ back/blueprints/user/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+from argon2 import PasswordHasher
+from models.db import db
+from models.users import Users
+
+users_bp = Blueprint('users_bp', __name__, url_prefix='/users')
+
+@users_bp.route('/register', methods=['PUT'])
+def register_user():
+    if request.data:
+        data = request.get_json()
+        ph = PasswordHasher()
+        hash = ph.hash(data['password'])
+        new_user = Users(data['name'], data['email'], data[hash])
+        db.session.add(new_user)
+        db.session.commit()
+    return jsonify({"status": "ok", "message": "user registered"})
