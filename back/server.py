06362d1c572f086c7f36a9db180bfcb1aa83519e
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-31 16:48:54 +0800

    Preparing to utilize Flask-RESTful
    -Utilize Flask-RESTful instead of blueprints to handle RESTful methods to specialize Flask endpoints for REST

--- back/server.py
+++ back/server.py
@@ -5,12 +5,8 @@ from flask import Flask, jsonify, request
 
 from models.db import db
 from schemas.mm import mm
+from flask_restful import Api
 
-# Import blueprints dictating route specific functions.
-from blueprints.admin.routes import admin_bp
-from blueprints.display.routes import display_bp
-from blueprints.users.routes import users_bp
-from blueprints.seed.routes import seed_bp
 
 # Load .env
 from dotenv import load_dotenv
@@ -23,26 +19,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI'
 # Initialize other app dependent instances
 db.init_app(app)
 mm.init_app(app)
+api = Api(app)
 
-# Register routes from blueprints
-app.register_blueprint(admin_bp)
-app.register_blueprint(display_bp)
-app.register_blueprint(users_bp)
-app.register_blueprint(seed_bp)
 
 @app.route("/")
 def get_data():
-    return jsonify({
-        "name": "Justinn",
-        "registration_date": datetime.datetime.now()
-    }
-)
+    return 'Server running!'
 
-# @app.route("/login", methods="POST")
-# @login_required
-# def login():
-#     if request.method == "POST":
-#         print(request)
 
 if __name__ == '__main__':
     pass
