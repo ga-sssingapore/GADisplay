866cec49f870f66dabc1caff9244b8a2fc3cef9a
SeptismJustinn <ohhoehoe@gmail.com>
2023-05-31 14:14:40 +0800

    Added marshmallow schemas

--- back/server.py
+++ back/server.py
@@ -4,10 +4,12 @@ import datetime
 from flask import Flask, jsonify, request
 
 from models.db import db
+from schemas.mm import mm
 
 # Import blueprints dictating route specific functions.
 from blueprints.admin.routes import admin_bp
 from blueprints.display.routes import display_bp
+from blueprints.users.routes import users_bp
 from blueprints.seed.routes import seed_bp
 
 # Load .env
@@ -20,10 +22,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI'
 
 # Initialize other app dependent instances
 db.init_app(app)
+mm.init_app(app)
 
 # Register routes from blueprints
 app.register_blueprint(admin_bp)
 app.register_blueprint(display_bp)
+app.register_blueprint(users_bp)
 app.register_blueprint(seed_bp)
 
 @app.route("/")
