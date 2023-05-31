3be029365f99aca555fb08f8ecaaa87614350b72
Tan Justinn <ohhoehoe@gmail.com>
2023-05-31 23:34:31 +0800

    Drafted edit and helm page

--- front/src/pages/AdminEditPage.jsx
+++ front/src/pages/AdminEditPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function AdminEditPage() {
-  return <div>AdminEditPage</div>;
+  return (
+    <div>
+      <div>Course Code</div>
+      <div>Course Type</div>
+      <div>Start Date</div>
+      <div>End Date</div>
+      <div>Days on campus</div>
+      <div>Start Time</div>
+      <div>End Time</div>
+      <div>Room</div>
+      <div>Import CSV</div>
+      <div>delete</div>
+    </div>
+  );
 }
 
 export default AdminEditPage;
--- front/src/pages/AdminHelmPage.jsx
+++ front/src/pages/AdminHelmPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function AdminHelmPage() {
-  return <div>AdminHelmPage</div>;
+  return (
+    <div>
+      <div>Name</div>
+      <div>email</div>
+      <div>change password</div>
+      <div>Admins</div>
+      <div>Users {"->"} Promote/Remove</div>
+      <div>Pending {"->"} Approve/Clear</div>
+    </div>
+  );
 }
 
 export default AdminHelmPage;
