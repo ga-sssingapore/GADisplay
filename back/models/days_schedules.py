68fc6ef1ead251584e8b08f09c1d76bdb54081ca
Tan Justinn <ohhoehoe@gmail.com>
2023-06-02 00:47:21 +0800

    Added DaysSchedules model
    -Subject to redesign

---
+++ back/models/days_schedules.py
@@ -0,0 +1,51 @@
+from models.db import db
+
+
+class DaysSchedules(db.Model):
+    __tablename__ = 'Days_Schedules'
+
+    combi = db.Column(db.String(20), primary_key=True, nullable=False)
+    mon = db.Column(db.Boolean, server_default=db.FetchedValue())
+    tue = db.Column(db.Boolean, server_default=db.FetchedValue())
+    wed = db.Column(db.Boolean, server_default=db.FetchedValue())
+    thu = db.Column(db.Boolean, server_default=db.FetchedValue())
+    fri = db.Column(db.Boolean, server_default=db.FetchedValue())
+    sat_o = db.Column(db.Boolean, server_default=db.FetchedValue())
+    sat_e = db.Column(db.Boolean, server_default=db.FetchedValue())
+    sun = db.Column(db.Boolean, server_default=db.FetchedValue())
+
+    def __init__(self, mon=False, tue=False, wed=False, thu=False, fri=False, sat_o=False, sat_e=False, sun=False):
+        combiStr = ''
+        if mon:
+            combiStr += 'Mo'
+            self.mon = mon
+        if tue:
+            combiStr += 'Tu'
+            self.tue = tue
+        if wed:
+            combiStr += 'We'
+            self.wed = wed
+        if thu:
+            combiStr += 'Th'
+            self.thu = thu
+        if fri:
+            combiStr += 'Fr'
+            self.fri = fri
+        if sat_o and sat_e:
+            combiStr += 'SA'
+            self.sat_o = sat_o
+            self.sat_e = sat_e
+        elif sat_o:
+            combiStr += 'SO'
+            self.sat_o = sat_o
+        elif sat_e:
+            combiStr += 'SE'
+            self.sat_e = sat_e
+        if sun:
+            combiStr += 'Su'
+            self.sun = sun
+
+        self.combi = combiStr
+
+    def __repr__(self):
+        return f'<DaysSchedules "{self.combi}">'
