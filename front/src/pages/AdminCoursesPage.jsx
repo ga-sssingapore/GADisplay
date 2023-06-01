6fd58968789e1103caf3705ec0d8a171ce208181
SeptismJustinn <ohhoehoe@gmail.com>
2023-06-01 13:58:47 +0800

    Added button to link to Add courses page

--- front/src/pages/AdminCoursesPage.jsx
+++ front/src/pages/AdminCoursesPage.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import styles from "./pages_css/AdminCoursesPage.module.css";
 import CoursesList from "./components/CoursesList";
+import { Link } from "react-router-dom";
 
 function AdminCoursesPage() {
   return (
     <div>
-      <div className={styles.filter_container}>
+      <div className={styles.container}>
         <h5>*Completed courses have already been filtered out</h5>
       </div>
       <CoursesList />
+      <Link to="/admin/courses/add">
+        <div className={styles.add_button}>Add Course(s)</div>
+      </Link>
     </div>
   );
 }
