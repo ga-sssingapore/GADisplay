417200037707f714e6714dce54cd635d97da6085
SeptismJustinn <ohhoehoe@gmail.com>
2023-06-01 11:41:07 +0800

    Added Weekalendar row to display courses/room

---
+++ front/src/pages/components/WeekalendarRow.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function WeekalendarRow(props) {
+  return (
+    <tr>
+      <td>{props.id}</td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  );
+}
+
+export default WeekalendarRow;
