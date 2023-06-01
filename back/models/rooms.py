10b8ae8779681ef062610645c2483b539530bdb9
Tan Justinn <ohhoehoe@gmail.com>
2023-06-02 00:53:40 +0800

    Added Rooms model

---
+++ back/models/rooms.py
@@ -0,0 +1,13 @@
+from models.db import db
+
+
+class Rooms(db.Model):
+    __tablename__='Rooms'
+
+    room = db.Column(db.Integer, primary_key=True, nullable=False)
+
+    def __init__(self, room):
+        self.room = room
+
+    def __repr__(self):
+        return f'<Room "{self.room}">'
