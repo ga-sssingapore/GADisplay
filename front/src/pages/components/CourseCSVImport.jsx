46ee9e83c21e2dd255f57f616a7ef260dc356e7f
Tan Justinn <ohhoehoe@gmail.com>
2023-06-03 03:39:29 +0800

    Input now clears when modal closed

--- front/src/pages/components/CourseCSVImport.jsx
+++ front/src/pages/components/CourseCSVImport.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Papa from "papaparse";
+import CourseCSVImportConfirmation from "./CourseCSVImportConfirmation";
 
 function CourseCSVImport() {
+  const [formComplete, setFormComplete] = useState(false);
+  const [csvData, setCsvData] = useState([]);
+  const fileInputRef = useRef();
   function handleCSV(event) {
     Papa.parse(event.target.files[0], {
       header: true,
       skipEmptyLines: true,
-      complete: (results) => console.log(results.data),
+      complete: (results) => {
+        setCsvData(results.data);
+        setFormComplete(true);
+      },
     });
   }
+
+  function handleCloseModal() {
+    fileInputRef.current.value = null;
+    setFormComplete(false);
+  }
+
   return (
     <div>
       <h4>Import CSV</h4>
-      <input type="file" id="import_csv" onChange={handleCSV} />
+      <input
+        type="file"
+        id="import_csv"
+        onChange={handleCSV}
+        ref={fileInputRef}
+      />
+      {formComplete && (
+        <CourseCSVImportConfirmation
+          closeModal={handleCloseModal}
+          csvData={csvData}
+        />
+      )}
     </div>
   );
 }
