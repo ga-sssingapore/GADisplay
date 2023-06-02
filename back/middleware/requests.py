3e7f19c86dcf0e5765d11319313fea68807ef489
Tan Justinn <ohhoehoe@gmail.com>
2023-06-02 17:34:22 +0800

    UsersSchema includes Foreign Key
    -Minor formatting in requests.py

--- back/middleware/requests.py
+++ back/middleware/requests.py
@@ -6,6 +6,6 @@ def check_request(endpoint):
     @wraps(endpoint)
     def decorator(*args, **kwargs):
         if not request.data:
-            return jsonify({'status':'error', 'message':'invalid request data'})
+            return jsonify({'status': 'error', 'message': 'invalid request data'})
         return endpoint(*args, **kwargs)
     return decorator
--- back/schemas/users.py
+++ back/schemas/users.py
@@ -1,8 +1,10 @@
-from marshmallow_sqlalchemy import column2field
+from marshmallow_sqlalchemy import fields
 from schemas.mm import mm
+from schemas.roles import RolesSchema
 from models.users import Users
 
 
 class UsersSchema(mm.SQLAlchemyAutoSchema):
     class Meta:
         model = Users
+        include_fk = True
