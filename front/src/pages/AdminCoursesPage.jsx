9bc24547900240a34b9b2ec55e4e9c7e151c15fd
Tan Justinn <ohhoehoe@gmail.com>
2023-06-03 16:43:07 +0800

    AdminCoursesPage will fetchdata
    -Allow CoursesList to be more reusable by accepting variable courses array instead

--- front/src/pages/AdminCoursesPage.jsx
+++ front/src/pages/AdminCoursesPage.jsx
@@ -1,15 +1,192 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./pages_css/AdminCoursesPage.module.css";
 import CoursesList from "./components/CoursesList";
 import { Link } from "react-router-dom";
 
 function AdminCoursesPage() {
+  const [courses, setCourses] = useState([
+    {
+      cohort: "UXDI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-02T17:30"),
+      classroom: 6,
+    },
+    {
+      cohort: "DSI36SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 5,
+    },
+    {
+      cohort: "SEI43SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 3,
+    },
+    {
+      cohort: "UXDI45SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-21T17:30"),
+      classroom: 1,
+    },
+    {
+      cohort: "SEI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-28T17:30"),
+      classroom: 2,
+    },
+    {
+      cohort: "UXDI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-02T17:30"),
+      classroom: 6,
+    },
+    {
+      cohort: "DSI36SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 5,
+    },
+    {
+      cohort: "SEI43SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 3,
+    },
+    {
+      cohort: "UXDI45SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-21T17:30"),
+      classroom: 1,
+    },
+    {
+      cohort: "SEI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-28T17:30"),
+      classroom: 2,
+    },
+    {
+      cohort: "UXDI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-02T17:30"),
+      classroom: 6,
+    },
+    {
+      cohort: "DSI36SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 5,
+    },
+    {
+      cohort: "SEI43SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 3,
+    },
+    {
+      cohort: "UXDI45SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-21T17:30"),
+      classroom: 1,
+    },
+    {
+      cohort: "SEI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-28T17:30"),
+      classroom: 2,
+    },
+    {
+      cohort: "UXDI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-02T17:30"),
+      classroom: 6,
+    },
+    {
+      cohort: "DSI36SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 5,
+    },
+    {
+      cohort: "SEI43SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 3,
+    },
+    {
+      cohort: "UXDI45SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-21T17:30"),
+      classroom: 1,
+    },
+    {
+      cohort: "SEI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-28T17:30"),
+      classroom: 2,
+    },
+    {
+      cohort: "UXDI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-02T17:30"),
+      classroom: 6,
+    },
+    {
+      cohort: "DSI36SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 5,
+    },
+    {
+      cohort: "SEI43SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-06-09T17:30"),
+      classroom: 3,
+    },
+    {
+      cohort: "UXDI45SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-21T17:30"),
+      classroom: 1,
+    },
+    {
+      cohort: "SEI44SGP",
+      type: "Fulltime",
+      start: new Date("2023-05-17T09:30"),
+      end: new Date("2023-07-28T17:30"),
+      classroom: 2,
+    },
+  ]);
   return (
     <div>
       <div className={styles.container}>
         <h5>*Completed courses have already been filtered out</h5>
       </div>
-      <CoursesList />
+      <CoursesList courses={courses} />
       <Link to="/admin/courses/add">
         <div className={styles.add_button}>Add Course(s)</div>
       </Link>
