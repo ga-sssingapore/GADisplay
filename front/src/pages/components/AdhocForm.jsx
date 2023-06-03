277a6d2fa7cb0978f6077b3dbd11442a9b7664af
Tan Justinn <ohhoehoe@gmail.com>
2023-06-03 22:52:55 +0800

    Added AdhocForm

--- front/src/pages/components/AdhocForm.jsx
+++ front/src/pages/components/AdhocForm.jsx
@@ -1,15 +1,147 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import styles from "./css/AdhocForm.module.css";
+import AdhocFormConfirmation from "./AdhocFormConfirmation";
 
 function AdhocForm() {
+  const [event, setEvent] = useState("");
+  const [purpose, setPurpose] = useState("");
+  const [room, setRoom] = useState("");
+  const [date, setDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [formComplete, setFormComplete] = useState(false);
+
+  // UseRefs for validation
+  const eventRef = useRef();
+  const purposeRef = useRef();
+  const roomRef = useRef();
+  const dateRef = useRef();
+  const startRef = useRef();
+  const endRef = useRef();
+
+  function handleChange(event, stateSetter) {
+    stateSetter(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (eventRef.current.value == "") {
+      alert("Please enter event name! ");
+      return eventRef.current.focus();
+    } else if (purposeRef.current.value == "") {
+      alert("Please enter purpose!    ");
+      return purposeRef.current.focus();
+    } else if (dateRef.current.value == "") {
+      alert("Please select a date!    ");
+      return dateRef.current.focus();
+    } else if (roomRef.current.value == "") {
+      alert("Please select room!      ");
+      return roomRef.current.focus();
+    } else if (startRef.current.value == "") {
+      alert("Please select start time!");
+      return startRef.current.focus();
+    } else if (endRef.current.value == "") {
+      alert("Please select end time!  ");
+      return endRef.current.focus();
+    } else {
+      setFormComplete(true);
+    }
+  }
+
+  function handleConfirm(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      <div>Event</div>
-      <div>Date</div>
-      <div>Start Time</div>
-      <div>End Time</div>
-      <div>Room</div>
-      <div>Purpose</div>
-    </div>
+    <>
+      <div className={styles.form_container}>
+        <div className={styles.details_container}>
+          <label htmlFor="event">Event: </label>
+          <input
+            type="text"
+            id="event"
+            placeholder="Event name"
+            value={event}
+            onChange={(e) => handleChange(e, setEvent)}
+            ref={eventRef}
+          />
+          <label htmlFor="purpose">Purpose: </label>
+          <input
+            type="text"
+            id="purpose"
+            placeholder="Purpose of event"
+            value={purpose}
+            onChange={(e) => handleChange(e, setPurpose)}
+            ref={purposeRef}
+          />
+        </div>
+        <div className={styles.dateroom_container}>
+          <label htmlFor="date">Date</label>
+          <input
+            type="date"
+            id="date"
+            className={styles.datetime_selector}
+            min={new Date().toISOString().split("T")[0]}
+            value={date}
+            onChange={(e) => handleChange(e, setDate)}
+            ref={dateRef}
+          />
+          <label htmlFor="room">Room: </label>
+          <select
+            id="room"
+            value={room}
+            onChange={(e) => handleChange(e, setRoom)}
+            className={styles.room_selector}
+            ref={roomRef}
+          >
+            <option value="" disabled hidden>
+              --
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+          </select>
+          <label htmlFor="start">Start Time</label>
+          <input
+            type="time"
+            id="start"
+            className={styles.datetime_selector}
+            value={startTime}
+            onChange={(e) => handleChange(e, setStartTime)}
+            ref={startRef}
+          />
+          <label htmlFor="end">End Time</label>
+          <input
+            type="time"
+            id="end"
+            className={styles.datetime_selector}
+            value={endTime}
+            onChange={(e) => handleChange(e, setEndTime)}
+            ref={endRef}
+          />
+        </div>
+        <div>
+          <button className={styles.button} onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </div>
+      {formComplete && (
+        <AdhocFormConfirmation
+          event={event}
+          purpose={purpose}
+          room={room}
+          date={date}
+          startTime={startTime}
+          endTime={endTime}
+          setFormComplete={setFormComplete}
+          handleConfirm={handleConfirm}
+        />
+      )}
+    </>
   );
 }
 
