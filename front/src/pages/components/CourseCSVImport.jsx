e627ea77f59580f06e894fc8ee5da5f0c2d7817d
Tan Justinn <ohhoehoe@gmail.com>
2023-06-03 14:01:35 +0800

    Added loading spinner + CSS
    -Resized modal and allowed vertical scrolling
    -Adjusted size of elements within form

--- front/src/pages/components/CourseCSVImport.jsx
+++ front/src/pages/components/CourseCSVImport.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useRef } from "react";
 import Papa from "papaparse";
 import CourseCSVImportConfirmation from "./CourseCSVImportConfirmation";
+import styles from "./css/CourseCSVImport.module.css";
 
 function CourseCSVImport() {
   const [formComplete, setFormComplete] = useState(false);
   const [csvData, setCsvData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const fileInputRef = useRef();
   function handleCSV(event) {
+    setLoading(true);
     Papa.parse(event.target.files[0], {
       header: true,
       skipEmptyLines: true,
       complete: (results) => {
         setCsvData(results.data);
+        setLoading(false);
         setFormComplete(true);
       },
     });
@@ -23,21 +27,30 @@ function CourseCSVImport() {
   }
 
   return (
-    <div>
-      <h4>Import CSV</h4>
-      <input
-        type="file"
-        id="import_csv"
-        onChange={handleCSV}
-        ref={fileInputRef}
-      />
+    <>
+      <div className={styles.container}>
+        <h4>Import CSV</h4>
+        <input
+          type="file"
+          id="import_csv"
+          onChange={handleCSV}
+          ref={fileInputRef}
+        />
+        {loading && (
+          <img
+            src="/GALogo.png"
+            alt="GA logo loading"
+            className={styles.loading_spinner}
+          />
+        )}
+      </div>
       {formComplete && (
         <CourseCSVImportConfirmation
           closeModal={handleCloseModal}
           csvData={csvData}
         />
       )}
-    </div>
+    </>
   );
 }
 
