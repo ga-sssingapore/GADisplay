50a97cf1443f68d5f1ec79aec9f2d58e0bbd17ad
Tan Justinn <ohhoehoe@gmail.com>
2023-06-04 20:07:49 +0800

    Admin page can fetch all users now
    -Different functionality depending on role
    -Admins can promote/approve/delete users
    -Users can view
    -Registered cannot view anything

--- front/src/pages/AdminAdministrationPage.jsx
+++ front/src/pages/AdminAdministrationPage.jsx
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import UserContext from "../context/user";
+import styles from "./pages_css/AdminAdministrationPage.module.css";
+import { fetchData } from "../helpers/common";
+import UsersList from "./components/UsersList";
 
 function AdminAdministrationPage() {
+  const userCtx = useContext(UserContext);
+  const [admins, setAdmins] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [pending, setPending] = useState([]);
+
+  async function getAllUsers() {
+    try {
+      const { ok, data } = await fetchData("/auth/users", userCtx.accessToken);
+      if (ok) {
+        setAdmins(data.filter((item) => item.role == "Admin"));
+        setUsers(data.filter((item) => item.role == "User"));
+        setPending(data.filter((item) => item.role == "Registered"));
+      } else {
+        throw new Error(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+      alert("Error getting users");
+    }
+  }
+
+  useEffect(() => {
+    if (userCtx.accessToken != "") {
+      getAllUsers();
+    }
+  }, [userCtx.accessToken]);
+
   return (
-    <div>
-      <div>Name</div>
-      <div>email</div>
-      <div>change password</div>
-      <div>Admins</div>
-      <div>Users {"->"} Promote/Remove</div>
-      <div>Pending {"->"} Approve/Clear</div>
+    <div className={styles.container}>
+      <div className={styles.account}>
+        <div>Name: {userCtx.claims.name}</div>
+        <div>Email: {userCtx.claims.email}</div>
+      </div>
+      <hr />
+      {/* <div>change password</div> */}
+      <div className={styles.users_lists}>
+        <div>
+          <UsersList users={users} role="User" />
+        </div>
+        <div>
+          <UsersList users={admins} />
+        </div>
+      </div>
+      <hr />
+      <UsersList users={pending} role="Registered" />
     </div>
   );
 }
