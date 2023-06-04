2c3179742ada0cee734ad9b08540d9c9f9bc6c25
Tan Justinn <ohhoehoe@gmail.com>
2023-06-04 22:45:20 +0800

    Added delete endpoint

--- back/resources/users.py
+++ back/resources/users.py
@@ -1,11 +1,11 @@
 from flask import jsonify, request
-from jwt import ExpiredSignatureError
 from flask_jwt_extended import jwt_required, get_jwt
 from middleware.requests import check_request
 from flask_restful import Resource
 
 from models.db import db
 from models.users import Users
+from models.logins import Logins
 from schemas.users import UsersSchema
 
 
@@ -46,6 +46,23 @@ class UsersEP(Resource):
             db.session.query(Users).filter_by(id=data['id']).update({'role': 'User'})
             db.session.commit()
             return jsonify({'status': 'ok', 'message': 'user approved'})
-        except (Exception, ExpiredSignatureError) as e:
+        except Exception as e:
             print(e)
             return jsonify({'status': 'error', 'message': 'error approving user'}), 400
+
+    @classmethod
+    @check_request
+    @jwt_required()
+    def delete(cls):
+        admin_role = get_jwt()['role']
+        if admin_role != "Admin":
+            return jsonify({'status': 'error', 'message': 'admin-only function'}), 403
+        try:
+            data = request.get_json()
+            db.session.query(Logins).filter_by(id=data['id']).delete()
+            db.session.query(Users).filter_by(id=data['id']).delete()
+            db.session.commit()
+            return jsonify({'status': 'ok', 'message': 'user deleted'})
+        except Exception as e:
+            print(e)
+            return jsonify({'status': 'error', 'message': 'error deleting user'}), 400
