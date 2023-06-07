1edad27e383ab9e76e332fb5566ae4d6238c2ef6
SeptismJustinn <ohhoehoe@gmail.com>
2023-06-07 10:59:40 +0800

    Updated timer logic
    -Client-side update every X minutes, specified by .env (intended to be 15 mins for now)
    -Server-side update every Y minutes, specified by.env(intended to be 60 mins for now)
    -Reset timer everytime server-side updates, in case of timer desync from device time due to lags in code

--- front/src/pages/DisplayPage.jsx
+++ front/src/pages/DisplayPage.jsx
@@ -8,28 +8,44 @@ function DisplayPage() {
   const location = useLocation();
   const { admin } = location.state;
   const navigate = useNavigate();
+  // Ordered by start date, only first cohort displayed
   const [cohorts, setCohorts] = useState([]);
+  // Ordered by creation order (whichever the database receives first, in lieu of creation date)
+  // If reordering required, delete adhocs and re-create in order?
   const [adhocs, setAdhocs] = useState([]);
   // Currently displayed class, update every 10mins?
   const [display, setDisplay] = useState({});
   let timeout = "";
   let interval = "";
 
-  async function getDisplay() {
-    startInterval(() => null);
+  /* --- Display controls --- */
+  function updateDisplay(refreshMinutes) {
+    if (new Date() % refreshMinutes <= 60000) {
+      // If about time to refresh, fetch data before changing
+      getDisplay();
+    } else {
+      changeDisplay();
+    }
+  }
 
+  async function getDisplay() {
     try {
       const { ok, data } = await fetchData("/display/", undefined, "POST", {
         now: new Date(),
         room: number,
       });
       if (ok) {
-        if (interval === "") {
-          startInterval(getDisplay);
+        // Reset interval
+        if (interval != "") {
+          clearInterval(interval);
+          interval = "";
         }
+        startInterval(updateDisplay);
+        console.log(data);
+        // Retrieve cohorts that have classes today
         setCohorts(data.cohort);
+        // Retrieve all cohorts that have yet to end at this time
         setAdhocs(data.adhoc);
-        changeDisplay();
       } else {
         throw new Error(data);
       }
@@ -38,14 +54,61 @@ function DisplayPage() {
     }
   }
 
+  // Call changeDisplay everytime adhocs/cohorts change (i.e. when they get fetched)
+  useEffect(() => {
+    changeDisplay();
+  }, [adhocs, cohorts]);
+
   function changeDisplay() {
+    console.log("test", adhocs);
     if (adhocs.length > 0) {
-      setDisplay({});
-    } else if (cohorts.length > 0) {
-      setDisplay(cohorts[0]);
+      /* To account for multiple adhocs happening between data refreshes,
+      check which adhoc is happening within this update and display. */
+      console.log(adhocs[0]);
+      adhocs.splice(0, 1);
+      console.log(adhocs[0]);
+      // setDisplay(adhocs[0]);
+      console.log(adhocs);
+      return;
     }
+    if (cohorts.length > 0) {
+      // If no adhocs returned, set first available cohort.
+      setDisplay(cohorts[0]);
+      return;
+    }
+    // If no adhocs/cohorts, clear display
+    setDisplay({});
   }
 
+  function startInterval(fn) {
+    const refreshMinutes = 60000 * import.meta.env.VITE_SERVERREFRESHTIMER;
+    // Value
+    const updateMinutes = 60000 * import.meta.env.VITE_CLIENTREFRESHTIMER;
+    const msToUpdate = updateMinutes - (new Date() % updateMinutes);
+    console.log(msToUpdate);
+    // Reset timeout
+    if (timeout != "") {
+      clearTimeout(timeout);
+      timeout = "";
+    }
+    timeout = setTimeout(() => {
+      // Update display client-side every 15 minutes
+      interval = setInterval(fn, 60000 * 15, refreshMinutes);
+    }, msToUpdate);
+  }
+
+  useEffect(() => {
+    getDisplay();
+    // Clean up rubbish before you leave the theater!
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+      interval = "";
+      timeout = "";
+    };
+  }, []);
+
+  /* --- Display formatting functions --- */
   function getTime(dateStr) {
     if (!dateStr) {
       return;
@@ -57,30 +120,6 @@ function DisplayPage() {
     }
     return timeFirstHalf + timeArr[1];
   }
-
-  function startInterval(fn) {
-    const refreshMinutes = 60000 * import.meta.env.VITE_REFRESHTIMER;
-    const msToRefresh = refreshMinutes - (new Date() % refreshMinutes);
-    if (timeout === "") {
-      timeout = setTimeout(() => {
-        interval = setInterval(fn, refreshMinutes);
-      }, msToRefresh);
-    }
-  }
-
-  useEffect(() => {
-    getDisplay();
-    return () => {
-      clearInterval(interval);
-      clearTimeout(timeout);
-      interval = "";
-      timeout = "";
-    };
-  }, []);
-
-  useEffect(() => {
-    changeDisplay();
-  }, [adhocs, cohorts]);
 
   return (
     <div className={styles.background}>
