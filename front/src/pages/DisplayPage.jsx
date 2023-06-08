08af95e24534874db6b06b5a24027e57e4b48f0a
Tan Justinn <ohhoehoe@gmail.com>
2023-06-08 22:15:04 +0800

    Fixed interval
    -SetState((state)=>nextState) mandatory to allow interval/timeout to work with hooks in reactive component. Alternative is to lag the component so that there is enough time for useEffect to trigger instead? (inconsistent and stupid strategy)
    -Utilized timeout chains instead of setInterval so that potential bugs can be dealt with by addressing Timeouts alone
    -Moved refreshMinutes and updateMinutes to component-wide variables since they are calculated similarly throughout

--- front/src/pages/DisplayPage.jsx
+++ front/src/pages/DisplayPage.jsx
@@ -15,48 +15,53 @@ function DisplayPage() {
   const [adhocs, setAdhocs] = useState([]);
   // Currently displayed class, update every 10mins?
   const [display, setDisplay] = useState({});
+  // Env variables
+  const refreshMinutes = 60000 * import.meta.env.VITE_SERVERREFRESHTIMER;
+  const updateMinutes = 60000 * import.meta.env.VITE_CLIENTREFRESHTIMER;
 
   /* --- Timer --- */
   let timeout = "";
-  let interval = "";
-  function startInterval(fn) {
-    const refreshMinutes = 60000 * import.meta.env.VITE_SERVERREFRESHTIMER;
-    // Value
-    const updateMinutes = 60000 * import.meta.env.VITE_CLIENTREFRESHTIMER;
-    const msToUpdate = updateMinutes - (new Date() % updateMinutes);
+  function startInterval() {
     // Reset timeout
     if (timeout != "") {
       clearTimeout(timeout);
       timeout = "";
     }
+    // 1s allowance to account for possible system lag
+    const msToUpdate = updateMinutes - (new Date() % updateMinutes);
     timeout = setTimeout(() => {
       // Update display client-side every 15 minutes
-      changeDisplay();
-      interval = setInterval(fn, 60000 * 15, refreshMinutes);
+      updateDisplay();
+      nextInterval();
     }, msToUpdate);
-    console.log(timeout, msToUpdate);
+  }
+
+  function nextInterval() {
+    timeout = setTimeout(() => {
+      updateDisplay();
+      nextInterval();
+    }, updateMinutes);
   }
 
   /* --- Display controls --- */
   function changeDisplay() {
-    console.log("changing");
+    const now = new Date();
     if (adhocs.length > 0) {
       /* To account for multiple adhocs happening between data refreshes,
       check which adhoc is happening within this update and display. */
       const currentAdhoc = adhocs[0];
-      console.log(adhocs);
-      if (new Date(currentAdhoc.ends) - new Date() > 0) {
+      const nextAdhocs = adhocs.toSpliced(0, 1);
+      if (new Date(currentAdhoc.ends) - now > 1000) {
         // If currentAdhoc yet to end, check if it has started OR is about to start in the next client refresh.
         if (
-          new Date(currentAdhoc.starts) - new Date() <=
+          new Date(currentAdhoc.starts) - now <=
           60000 * import.meta.env.VITE_CLIENTREFRESHTIMER
         ) {
           return setDisplay(currentAdhoc);
         }
       } else {
         // If currentAdhoc has expired, truncate adhocs and recurse via useEffect
-        console.log("recurse", adhocs);
-        return setAdhocs(adhocs.toSpliced(0, 1));
+        return setAdhocs((adhoc) => adhoc.toSpliced(0, 1));
       }
     }
     if (cohorts.length > 0) {
@@ -67,8 +72,7 @@ function DisplayPage() {
     return setDisplay({});
   }
 
-  function updateDisplay(refreshMinutes) {
-    console.log("updating");
+  function updateDisplay() {
     if (new Date() % refreshMinutes <= 60000) {
       // If about time to refresh, fetch data before changing
       getDisplay();
@@ -78,20 +82,13 @@ function DisplayPage() {
   }
 
   async function getDisplay() {
-    console.log("Fetching");
     try {
       const { ok, data } = await fetchData("/display/", undefined, "POST", {
         now: new Date(),
         room: number,
       });
       if (ok) {
-        console.log("Fetched: ", data);
-        // Reset interval
-        if (interval != "") {
-          clearInterval(interval);
-          interval = "";
-        }
-        startInterval(updateDisplay);
+        startInterval();
         // Retrieve cohorts that have classes today
         setCohorts(data.cohort);
         // Retrieve all cohorts that have yet to end at this time
@@ -115,9 +112,7 @@ function DisplayPage() {
     getDisplay();
     // Clean up rubbish before you leave the theater!
     return () => {
-      clearInterval(interval);
       clearTimeout(timeout);
-      interval = "";
       timeout = "";
     };
   }, []);
